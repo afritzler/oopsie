5d5b8206abd7650550a7d56cd53572af167e3cd2
Andreas Fritzler <andreas.fritzler@sap.com>
2019-12-09 18:01:32 +0100

    First working version

--- controller.go
+++ controller.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
 	"github.com/go-logr/logr"
+	"k8s.io/client-go/tools/record"
 
-	appsv1 "k8s.io/api/apps/v1"
+	"io/ioutil"
+
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -14,8 +21,17 @@ import (
 // reconcileReplicaSet reconciles ReplicaSets
 type reconcileReplicaSet struct {
 	// client can be used to retrieve objects from the APIServer.
-	client client.Client
-	log    logr.Logger
+	client   client.Client
+	log      logr.Logger
+	recorder record.EventRecorder
+}
+
+type Answers struct {
+	Items []Item `json:"items,omitempty`
+}
+
+type Item struct {
+	Link string `json:"link,omitempty"`
 }
 
 // Implement reconcile.Reconciler so the controller can reconcile objects
@@ -25,37 +41,76 @@ func (r *reconcileReplicaSet) Reconcile(request reconcile.Request) (reconcile.Re
 	// set up a convenient log object so we don't have to type request over and over again
 	log := r.log.WithValues("request", request)
 
-	// Fetch the ReplicaSet from the cache
-	rs := &appsv1.ReplicaSet{}
-	err := r.client.Get(context.TODO(), request.NamespacedName, rs)
+	event := &corev1.Event{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, event)
 	if errors.IsNotFound(err) {
-		log.Error(nil, "Could not find ReplicaSet")
+		log.Error(nil, "Could not find Events")
 		return reconcile.Result{}, nil
 	}
 
 	if err != nil {
-		log.Error(err, "Could not fetch ReplicaSet")
+		log.Error(err, "Could not fetch Events")
 		return reconcile.Result{}, err
 	}
 
-	// Print the ReplicaSet
-	log.Info("Reconciling ReplicaSet", "container name", rs.Spec.Template.Spec.Containers[0].Name)
+	// Output found events
+	log.V(2).Info(fmt.Sprintf("found events: event %s with reason %s", event.Type, event.Reason))
 
-	// Set the label if it is missing
-	if rs.Labels == nil {
-		rs.Labels = map[string]string{}
-	}
-	if rs.Labels["hello"] == "world" {
-		return reconcile.Result{}, nil
+	if event.Type == corev1.EventTypeWarning && event.Message != "" {
+		log.Info(fmt.Sprintf("found event with warning: event %s with reason %s", event.Type, event.Reason))
+		fmt.Printf("%s\n", event.Message)
+		req, err := http.NewRequest("GET", "https://api.stackexchange.com/2.2/search", nil)
+		if err != nil {
+			log.Error(err, "failed to construct request")
+			return reconcile.Result{}, err
+		}
+		q := req.URL.Query()
+		q.Add("order", "desc")
+		q.Add("sort", "votes")
+		q.Add("intitle", event.Message)
+		q.Add("site", "stackoverflow")
+		req.URL.RawQuery = q.Encode()
+
+		fmt.Println(req.URL.String())
+
+		resp, err := http.Get(req.URL.String())
+		if err != nil {
+			log.Error(err, "failed to query backend")
+			return reconcile.Result{}, err
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Error(err, "failed to get response body")
+			return reconcile.Result{}, err
+		}
+
+		//fmt.Printf("%+v", string(body))
+
+		var anyJSON Answers
+		json.Unmarshal(body, &anyJSON)
+
+		if len(anyJSON.Items) > 0 && anyJSON.Items[0].Link != "" {
+			link := anyJSON.Items[0].Link
+			log.Info("Fired RS event")
+			r.recorder.Event(&event.InvolvedObject, v1.EventTypeNormal, "Hint", link)
+		}
+
 	}
 
-	// Update the ReplicaSet
-	rs.Labels["hello"] = "world"
-	err = r.client.Update(context.TODO(), rs)
-	if err != nil {
-		log.Error(err, "Could not write ReplicaSet")
-		return reconcile.Result{}, err
-	}
+	//// Update the ReplicaSet
+	//rs.Labels["hello"] = "world"
+	//err = r.client.Update(context.TODO(), rs)
+	//if err != nil {
+	//	log.Error(err, "Could not write ReplicaSet")
+	//	return reconcile.Result{}, err
+	//} else {
+	//	log.Info("Fired RS event")
+	//	// Event(object runtime.Object, eventtype, reason, message string)
+	//	r.recorder.Event(rs, v1.EventTypeNormal, "TrallalaReason", "updated lables")
+	//}
 
 	return reconcile.Result{}, nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"k8s.io/client-go/kubernetes"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/record"
 	"os"
 
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
@@ -30,28 +32,56 @@ func main() {
 		os.Exit(1)
 	}
 
+	clientset, err := kubernetes.NewForConfig(mgr.GetConfig())
+	if err != nil {
+		panic("ooops failed to construct client")
+	}
+
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(log.Info)
+	eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: typedcorev1.New(clientset.CoreV1().RESTClient()).Events("")})
+	recorder := eventBroadcaster.NewRecorder(mgr.GetScheme(), corev1.EventSource{Component: "StackOverflow :-)"})
+
 	// Setup a new controller to reconcile ReplicaSets
 	entryLog.Info("Setting up controller")
 	c, err := controller.New("foo-controller", mgr, controller.Options{
-		Reconciler: &reconcileReplicaSet{client: mgr.GetClient(), log: log.WithName("reconciler")},
+		Reconciler: &reconcileReplicaSet{client: mgr.GetClient(), recorder: recorder, log: log.WithName("reconciler")},
 	})
 	if err != nil {
 		entryLog.Error(err, "unable to set up individual controller")
 		os.Exit(1)
 	}
 
-	// Watch ReplicaSets and enqueue ReplicaSet object key
-	if err := c.Watch(&source.Kind{Type: &appsv1.ReplicaSet{}}, &handler.EnqueueRequestForObject{}); err != nil {
-		entryLog.Error(err, "unable to watch ReplicaSets")
+	//informer := cache.NewSharedIndexInformer(&cache.ListWatch{
+	//	ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+	//		return clientset.CoreV1().Events("").List(options)
+	//	},
+	//	WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+	//		return clientset.CoreV1().Events("").Watch(options)
+	//	},
+	//},
+	//&corev1.Event{},
+	//0,
+	//cache.Indexers{})
+
+	// Watch Events and enqueue Events object key
+	if err := c.Watch(&source.Kind{Type: &corev1.Event{}}, &handler.EnqueueRequestForObject{}); err != nil {
+		entryLog.Error(err, "unable to watch events")
 		os.Exit(1)
 	}
 
-	// Watch Pods and enqueue owning ReplicaSet key
-	if err := c.Watch(&source.Kind{Type: &corev1.Pod{}},
-		&handler.EnqueueRequestForOwner{OwnerType: &appsv1.ReplicaSet{}, IsController: true}); err != nil {
-		entryLog.Error(err, "unable to watch Pods")
-		os.Exit(1)
-	}
+	//// Watch ReplicaSets and enqueue ReplicaSet object key
+	//if err := c.Watch(&source.Kind{Type: &appsv1.ReplicaSet{}}, &handler.EnqueueRequestForObject{}); err != nil {
+	//	entryLog.Error(err, "unable to watch ReplicaSets")
+	//	os.Exit(1)
+	//}
+	//
+	//// Watch Pods and enqueue owning ReplicaSet key
+	//if err := c.Watch(&source.Kind{Type: &corev1.Pod{}},
+	//	&handler.EnqueueRequestForOwner{OwnerType: &appsv1.ReplicaSet{}, IsController: true}); err != nil {
+	//	entryLog.Error(err, "unable to watch Pods")
+	//	os.Exit(1)
+	//}
 
 	entryLog.Info("starting manager")
 	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
